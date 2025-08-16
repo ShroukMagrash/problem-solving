c5ed682dc5ca43d8ac93016ab063717358e44415
ShroukMagrash <shroukashrafmagrash2@gmail.com>
2025-08-16 16:50:31 +0300

    print the Array

---
+++ print the Array.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int num ;
+    int arr[num];
+    cin >> num;
+    for (int i =0 ; i < num ;i++) {
+        cin >> arr[i] ;
+    }for (int i =0 ; i < num ;i++) {
+        cout << arr[i] << endl;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
